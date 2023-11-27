6199dfa25af11ef92badb4ff1a64d1c49e4bb76f
Jerry <jerryh25@gmail.com>
2023-11-27 14:36:21 -0700

    update js and index

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,9 @@
+let Country = 'United States';
+let Continent = 'North America';
+let Population = '10,000,000';
+console.log(Country);
+console.log(Continent);
+console.log(Population);
+
+
+console.log(10000000/2);
