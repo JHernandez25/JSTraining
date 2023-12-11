1fa765f7c4c28c0d68e43dcf5b28e2d7911ec14f
Jerry <jerryh25@gmail.com>
2023-12-11 15:52:44 -0700

    up to date

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -1,26 +1,35 @@
-const Country = 'United States';
-const Continent = 'North America';
-let Population = '10';
-const IsIsland = false;
-const language = 'English';
-//console.log (typeof Country);
-//console.log(typeof Continent);
-//console.log(typeof Population);
-//console.log(typeof IsIsland);
-//console.log(typeof language);
-/*
-console.log(Country);
-console.log(Continent);
-console.log(Population);
-console.log(Population/2);
-Population++;
-console.log(Population);
-console.log(IsIsland);
-console.log(language);
-console.log(Population > 6);
-console.log(Population < 33);
-*/
+let country = 'United States';
+let continent = 'North America';
+let population = 20;
+let IsIsland = false;
+let language;
+// console.log (country);
+// console.log (continent);
+// console.log (population);
 
-let description;
+/*
+console.log (typeof IsIsland);
+console.log (typeof population);
+console.log(typeof country);
+console.log (typeof population);
+console.log (typeof language);
+*/
+// Basic Operations in Javascript
+
+const now = 2037;
+const AgeJonah = now - 2004;
+const AgeSarah = now - 2010;
+
+console.log(AgeJonah, AgeSarah);
+console.log(AgeJonah / 2);
+
+//const firstName = 'Jonah';
+const LastName = 'Munoz';
+
+console.log(firstName + ' ' + LastName);
+
+
+
+
 
 
